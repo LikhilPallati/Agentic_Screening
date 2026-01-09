06e57c5c7e1def1402e8f4530c7cb4fc21708b28
Likhil Babu Pallati <131795952+LikhilPallati@users.noreply.github.com>
2026-01-09 01:18:29 -0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,10 +1,12 @@
 from pydantic import BaseModel
 from typing import List
 
+# Structure for the response
 class Candidate(BaseModel):
     name: str
     contact: dict
     skills: List[str]
     experience: List[str]
     education: List[str]
+
     score: float
